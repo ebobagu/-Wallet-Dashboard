ead1b46f9dffb20af2b5d930e26d4d4f7265103a
Bobby Agustinus Ginting <bobby.ginting@outlook.com>
2025-04-03 00:18:30 +0700

    Delete urls.py
    Signed-off-by: Bobby Agustinus Ginting <bobby.ginting@outlook.com>

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import get_wallet_balance, get_tokens, get_nfts
-
-urlpatterns = [
-    path("balance/<str:address>/", get_wallet_balance),
-    path("tokens/<str:address>/", get_tokens),
-    path("nfts/<str:address>/", get_nfts),
-]
